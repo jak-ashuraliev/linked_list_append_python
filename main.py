615fa660ee6fa77790c5a19b04936ae3abb1c03d
Jak Ashuraliev <ashuraliev@gmail.com>
2018-03-26 12:33:39 -0700

    Refactored code

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
-# A complete working Python program to demonstrate all
-# insertion methods of linked list
+# Linked list is a linear data structure and element are linked using pointers
+# Advantages? Dynamic size, saves memory, can live anywhere in the memory, ease of insertiong and deletion
 
-# Node class
+# Linked list consists of nodes
+# Each node consists of a value and a pointer to the next node
+# node => node => node => node = None
+
+# Starting node of linked list is referred to as head
+# Ending node of linked list is referred to as tail
+
+# Node class / function to initialize the node object
 class Node:
+	def __init__ (self, data):
+  	self.data = data # assing data
+    	self.next = None # initialize next as none/null
+    
+# LinkedList class / function to initialize the head = starting point
+class LinkedList(sefl):
+	# function to initialize the head
+ 	def __init__ (self):
+ 		self.head = None
+  
+ 	### Append Method ###
+  	def append(self, new_data):
+  	
+   	# Create a new node
+   	# Put in the data
+   	# Set next to None
+    	new_node = Node(new_data)
+    
+   	# Check if the linked list is empty, then make the new node as head
+   	if self.head is None:
+    		self.head = new_node
+     		return
+      
+    	# Else tarverse till the last node
+    	last = self.head
+    	while (last.next):
+    		last = last.next
+    
+    	# Change the next of last node
+    	last.next = new_node
+   
+	# Unitilty function to print the linked list
+  	def printList():
+  		temp = self.head
+    		while (temp):
+    		print temp.data, # print data
+      		temp = temp.next # point to next
 
-	# Function to initialise the node object
-	def __init__(self, data):
-		self.data = data # Assign data
-		self.next = None # Initialize next as null
+# Code execution statrs here
+if __name__ = '__main__':
 
-# Linked List class contains a Node object
-class LinkedList:
-
-	# Function to initialize head
-	def __init__(self):
-		self.head = None
-
-	# This function is defined in Linked List class
-	# Appends a new node at the end. This method is
-	# defined inside LinkedList class shown above */
-	def append(self, new_data):
-
-		# 1. Create a new node
-		# 2. Put in the data
-		# 3. Set next as None
-		new_node = Node(new_data)
-
-		# 4. If the Linked List is empty, then make the
-		# new node as head
-		if self.head is None:
-			self.head = new_node
-			return
-
-		# 5. Else traverse till the last node
-		last = self.head
-		while (last.next):
-			last = last.next
-
-		# 6. Change the next of last node
-		last.next = new_node
-
-
-	# Utility function to print the linked list
-	def printList(self):
-		temp = self.head
-		while (temp):
-			print temp.data,
-			temp = temp.next
-
-# Code execution starts here
-if __name__=='__main__':
-
-	# Start with the empty list
-	llist = LinkedList()
-
-	# Insert 5. So linked list becomes 5->None
+	# start with the empty list
+  	llist = LinkedList()
+  
+	# insert / append 5
 	llist.append(5)
 
-	# Insert 6 at the end. So linked list becomes 5->6->None
+	# insert / append 6
 	llist.append(6)
 
-	print 'Created linked list is:',
-	llist.printList()
+	#print the list
+	llist.printLisst()
